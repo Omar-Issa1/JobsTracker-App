a1eb41c1d2365cf600521910b0ef59c899b705a9
Omar Issa <omom2168@gmail.com>
2026-04-12 16:22:49 +0200

    Update MongoDB connection logging and change primary region in fly.toml

--- app.js
+++ app.js
@@ -69,12 +69,14 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, "0.0.0.0", () =>
-      console.log(`Server is listening on port ${port}...`),
-    );
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.log("MongoDB Error:", error);
   }
+
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is listening on port ${port}`);
+  });
 };
 
 start();
